2fa18e59468b8aaada9d9851e4e6702d39ff225e
MuhammadAbdullahSultan <abdullahsultan14@gmail.com>
2018-03-18 18:31:00 +0800

    +

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,5 +1,5 @@
 /*global angular*/
-var app = angular.module('gallery', ['ngRoute']);
+var app = angular.module('gallery', ['ngRoute', 'firebase']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     'use strict';
@@ -9,9 +9,8 @@ app.config(['$routeProvider', function ($routeProvider) {
         controller: 'galleryCtrl',
     });
 }]);
-
-app.controller('galleryCtrl', ['$scope', function ($scope) {
-    
+app.controller('galleryCtrl', ['$scope', 'firebase', '$firebaseArray', function ($scope, firebase, $firebaseArray) {
+    // GALLERY CODE
       jQuery(document).ready(function($) {
  
         $('#myCarousel').carousel({
@@ -35,5 +34,50 @@ app.controller('galleryCtrl', ['$scope', function ($scope) {
                 $('#carousel-text').html($('#slide-content-'+id).html());
         });
 });
+    
+    
+    // FIREBASE STORAGE CODE
+    
+    var storageRef = firebase.storage().ref();
+    var ref = firebase.database().ref();
+
+    $scope.vicHallImages = $firebaseArray(ref.child('images/vichall/'));
+
+    console.log($scope.vicHallImages);
+    
+    var progressbar = document.getElementById('upload');
+        var fileButton = document.getElementById("fileButton");
+        
+        fileButton.addEventListener('change', function (e) {
+            var file = e.target.files[0];
+            var pathReference = storageRef.child('images/vichall' + file.name);
+
+            var imagesRef = storageRef.child('images/vichall/' + file.name);
+            
+            imagesRef.put(file).then(function(snapshot) {
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                
+                $scope.vicHallImages.$add({
+                    url: snapshot.downloadURL
+                });
+                
+                upload.value = percentage;
+                
+                
+              console.log(snapshot.downloadURL);
+
+            });
+        });
+    
+    
+    $scope.update = function (id) {
+        console.log(id);
+        $scope.indexValue = $scope.vicHallImages.findIndex(vicHallImages => vicHallImages.$id === id);
+        console.log($scope.indexValue);
+    };
+    
+    
+    
+    
     
 }]);
