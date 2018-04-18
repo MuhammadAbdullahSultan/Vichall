34a16ed6e04a68b6e83f31f9449c352a26950440
MuhammadAbdullahSultan <abdullahsultan14@gmail.com>
2018-04-18 16:31:54 +0800

    +

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -144,81 +144,82 @@ app.controller('galleryCtrl', ['$scope', 'firebase', '$firebaseArray', 'toaster'
     
     // RESIDENTS -----------------------------------------------------------------------------------------------------------------
     
-    $scope.residentsImages = $firebaseArray(ref.child('images/residents/'));
-    
-    var progressbarResidents = document.getElementById('uploadResidents');
-        var fileButtonResidents = document.getElementById("fileButtonResidents");
-        
-        fileButtonResidents.addEventListener('change', function (e) {
-            
-            $scope.ResidentsImageUploaded = false;
-            $scope.ResidentsImageLoader = true;
-            $scope.ResidentsImageNotUploaded = false;
-            
-            $scope.$apply();
-            uploadResidents.value = 0;
-            var file = e.target.files[0];
-            
-            for (var i = 0 ; i < $scope.residentsImages.length ; i++) {
-                if($scope.residentsImages[i].filename === file.name) {
-                    $scope.ResidentsImageLoader = false;
-                    $scope.ResidentsImageNotUploaded = true;
-                    $scope.$apply();
-                    return;
-                }
-            }
-            var pathReference = storageRef.child('images/residents' + file.name);
-
-            var imagesRef = storageRef.child('images/residents/' + file.name);
-            
-            imagesRef.put(file).then(function(snapshot) {
-                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                $scope.ResidentsImageLoader = false;
-
-                $scope.residentsImages.$add({
-                    url: snapshot.downloadURL,
-                    filename: file.name,
-                    title: $scope.residentsImageTitle,
-                    description: $scope.residentsImageDescription
-                });
-                
-                uploadResidents.value = percentage;
-                $scope.ResidentsImageUploaded = true;
-
-            });
-        });
-    
-    $scope.deleteImageResidents = function () {
-        var desertRef = storageRef.child("images/residents/" + $scope.residentsImages[$scope.indexValueResidents].filename);
-        // Delete the file
-        desertRef.delete().then(function() {
-            
-            var item = $scope.residentsImages[$scope.indexValueResidents];
-            $scope.residentsImages.$remove(item).then(function (deletedData) {
-            });
-            toaster.pop({type: 'success', title: "OK", body: $scope.residentsImages[$scope.indexValueResidents].filename + " has been deleted"});
-
-          // File deleted successfully
-        }).catch(function(error) {
-            toaster.pop({type: 'danger', title: "Error", body: error});
-        });
-    }
-    
-    $scope.saveChangesResidents = function () {
-        $scope.residentsImages.$save($scope.indexValueResidents).then(function (data) {
-            console.log($scope.residentsImages[$scope.indexValueResidents]);
-            toaster.pop({type: 'success', title: "Ok",  body: $scope.residentsImages[$scope.indexValueResidents].filename + " has been edited" });
-        });
-    }
-    
-    $scope.updateResidents = function (id) {
-        $scope.indexValueResidents = $scope.residentsImages.findIndex(residentsImages => residentsImages.$id === id);
-    };
+//    $scope.residentsImages = $firebaseArray(ref.child('images/residents/'));
+//    
+//    var progressbarResidents = document.getElementById('uploadResidents');
+//        var fileButtonResidents = document.getElementById("fileButtonResidents");
+//        
+//        fileButtonResidents.addEventListener('change', function (e) {
+//            
+//            $scope.ResidentsImageUploaded = false;
+//            $scope.ResidentsImageLoader = true;
+//            $scope.ResidentsImageNotUploaded = false;
+//            
+//            $scope.$apply();
+//            uploadResidents.value = 0;
+//            var file = e.target.files[0];
+//            
+//            for (var i = 0 ; i < $scope.residentsImages.length ; i++) {
+//                if($scope.residentsImages[i].filename === file.name) {
+//                    $scope.ResidentsImageLoader = false;
+//                    $scope.ResidentsImageNotUploaded = true;
+//                    $scope.$apply();
+//                    return;
+//                }
+//            }
+//            var pathReference = storageRef.child('images/residents' + file.name);
+//
+//            var imagesRef = storageRef.child('images/residents/' + file.name);
+//            
+//            imagesRef.put(file).then(function(snapshot) {
+//                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//                $scope.ResidentsImageLoader = false;
+//
+//                $scope.residentsImages.$add({
+//                    url: snapshot.downloadURL,
+//                    filename: file.name,
+//                    title: $scope.residentsImageTitle,
+//                    description: $scope.residentsImageDescription
+//                });
+//                
+//                uploadResidents.value = percentage;
+//                $scope.ResidentsImageUploaded = true;
+//
+//            });
+//        });
+//    
+//    $scope.deleteImageResidents = function () {
+//        var desertRef = storageRef.child("images/residents/" + $scope.residentsImages[$scope.indexValueResidents].filename);
+//        // Delete the file
+//        desertRef.delete().then(function() {
+//            
+//            var item = $scope.residentsImages[$scope.indexValueResidents];
+//            $scope.residentsImages.$remove(item).then(function (deletedData) {
+//            });
+//            toaster.pop({type: 'success', title: "OK", body: $scope.residentsImages[$scope.indexValueResidents].filename + " has been deleted"});
+//
+//          // File deleted successfully
+//        }).catch(function(error) {
+//            toaster.pop({type: 'danger', title: "Error", body: error});
+//        });
+//    }
+//    
+//    $scope.saveChangesResidents = function () {
+//        $scope.residentsImages.$save($scope.indexValueResidents).then(function (data) {
+//            console.log($scope.residentsImages[$scope.indexValueResidents]);
+//            toaster.pop({type: 'success', title: "Ok",  body: $scope.residentsImages[$scope.indexValueResidents].filename + " has been edited" });
+//        });
+//    }
+//    
+//    $scope.updateResidents = function (id) {
+//        $scope.indexValueResidents = $scope.residentsImages.findIndex(residentsImages => residentsImages.$id === id);
+//    };
     
     // EVENTS -----------------------------------------------------------------------------------------------------------------
     
     $scope.eventsImages = $firebaseArray(ref.child('images/events/'));
-    
+    $scope.order_item = 'title';
+$scope.order_reverse = false;
     var progressbarEvents = document.getElementById('uploadEvents');
         var fileButtonEvents = document.getElementById("fileButtonEvents");
         
@@ -384,3 +385,17 @@ app.filter('range', function () {
         return input;
     };
     });
+
+app.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
